a1ca8f72c33f622873fcf8c86481c891d898e70a
jihadable <umarjihad141004@gmail.com>
2023-08-19 10:46:35 +0800

    compress some images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero({page, header}){
 
     return (
         <div className={`${page}-hero flex justify-center items-center bg-center bg-cover w-full h-[100vh] bg-fixed`}>
-            <div className={`tagline font-bold text-5xl text-greenCustome [text-shadow:2px_0_10px_#000] mobile:text-2xl`}>{header}</div>
+            <div className={`tagline font-bold text-5xl text-greenCustome mobile:text-2xl`}>{header}</div>
         </div>
     )
 }
