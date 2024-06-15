d6a9358937ce74b28e187ed3bba7e7c1f1c6cf2d
jihadable <umarjihad141004@gmail.com>
2024-06-15 22:59:40 +0700

    refactor payment status route

--- src/pages/PaymentStatus.jsx
+++ src/pages/PaymentStatus.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function PaymentStatus(){
     const navigate = useNavigate()
 
-    navigate("/history")
+    useEffect(() => {
+        navigate("/history")
+    }, [navigate])
+    
+    return null
 }
