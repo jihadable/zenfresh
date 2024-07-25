e24f7dc2a6e34243b4a02d1c332eaa6ae114dec7
jihadable <umarjihad141004@gmail.com>
2024-07-25 09:52:08 +0700

    uninstall react-calendar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import Router from './Router'
-import "./styles/Calender.css"
 import "./styles/ReactToastify.css"
 import "./styles/index.css"
 import "./styles/style.css"
