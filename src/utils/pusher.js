def14f6a72173d30595607c1e2ef1d982f6df083
jihadable <umarjihad141004@gmail.com>
2026-01-16 07:38:24 +0700

    update pusher

--- src/utils/pusher.js
+++ src/utils/pusher.js
@@ -1,8 +1,8 @@
 import Pusher from "pusher-js"
 
 const jwt = localStorage.getItem("jwt")
-export const pusher = new Pusher("8df6d4d768d779f1f8b3", {
-    cluster: "ap1",
+export const pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+    cluster: import.meta.env.VITE_PUSHER_CLUSTER,
     authEndpoint: import.meta.env.VITE_PUSHER_AUTH_ENDPOINT,
     auth: {
         headers: {
